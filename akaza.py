2e9ad5e609c22738bc4a0f22d06a1d2e4f50e73e
David Chinedu <144693026+DEMON-XD@users.noreply.github.com>
2024-10-04 09:58:46 +0100

    Update akaza.py

--- akaza.py
+++ akaza.py
@@ -1,9 +1,9 @@
 import os,platform
 os.system("git pull")
-os.system("xdg-open https://chat.whatsapp.com/D3Xd6IztOyi9Jg53G98Cjx")
+##os.system("xdg-open https://chat.whatsapp.com/D3Xd6IztOyi9Jg53G98Cjx")
 os.system('clear')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import DEMON
+    import DemoPaid
 elif bit=='32bit':
-    import DEMON32
+    import DemoPaid
